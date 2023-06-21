a4498aaaf53686f562b4332870fdac2d42727713
kl3eo <alex@motivation.ru>
2023-06-21 15:13:10 +0300

    fix page init to display lots unconnected

--- src/components/providers/Web3Provider.js
+++ src/components/providers/Web3Provider.js
@@ -13,7 +13,8 @@ const contextDefaultValues = {
   marketplaceContract: null,
   nftContract: null,
   isReady: false,
-  hasWeb3: false
+  hasWeb3: false,
+  hasInit: false
 }
 
 const networkNames = {
@@ -33,23 +34,29 @@ export default function Web3Provider ({ children }) {
   const [marketplaceContract, setMarketplaceContract] = useState(contextDefaultValues.marketplaceContract)
   const [nftContract, setNFTContract] = useState(contextDefaultValues.nftContract)
   const [isReady, setIsReady] = useState(contextDefaultValues.isReady)
+  const [hasInit, setHasInit] = useState(contextDefaultValues.hasInit)
 
   useEffect(() => {
     initializeWeb3()
   }, [])
 
+  async function checkConnection () {
+    return window.ethereum.request({ method: 'eth_accounts' })
+  }
+
   async function initializeWeb3WithoutSigner () {
     const providerURL = 'https://node.expanse.tech'
     const myProvider = ethers.getDefaultProvider(providerURL)
-    // console.log('here1', myProvider)
-    // const alchemyProvider = new ethers.providers.AlchemyProvider(2)
+    setHasInit(true)
     setHasWeb3(false)
     await getAndSetWeb3ContextWithoutSigner(myProvider)
   }
 
   async function initializeWeb3 () {
     try {
-      if (!window.ethereum) {
+      // console.log('here ethereum', window.ethereum)
+      const accs = await checkConnection()
+      if (!window.ethereum || (window.ethereum && !accs.length && !hasInit)) {
         await initializeWeb3WithoutSigner()
         return
       }
@@ -58,21 +65,16 @@ export default function Web3Provider ({ children }) {
       const web3Modal = new Web3Modal()
       const connection = await web3Modal.connect()
       setHasWeb3(true)
+      setHasInit(true)
       const myProvider = new ethers.providers.Web3Provider(connection, 'any')
-      // const providerURL = 'https://node.expanse.tech'
-      // const myProvider = ethers.getDefaultProvider(providerURL)
-      // console.log('here2', myProvider)
       await getAndSetWeb3ContextWithSigner(myProvider)
 
       function onAccountsChanged (accounts) {
         // Workaround to accountsChanged metamask mobile bug
         if (onAccountsChangedCooldown) return
         onAccountsChangedCooldown = true
-        // accounts = await provider.send("eth_requestAccounts", []);
         setTimeout(() => { onAccountsChangedCooldown = false }, 1000)
-        // console.log('here6', accounts[0])
         const changedAddress = ethers.utils.getAddress(accounts[0])
-        // console.log('here7', changedAddress)
         return getAndSetAccountAndBalance(myProvider, changedAddress)
       }
 
@@ -88,10 +90,8 @@ export default function Web3Provider ({ children }) {
     setIsReady(false)
     const signer = provider.getSigner()
     const signerAddress = await signer.getAddress()
-    // console.log('here8', signerAddress)
     await getAndSetAccountAndBalance(provider, signerAddress)
     const networkName = await getAndSetNetwork(provider)
-    // console.log('here3', networkName)
     const success = await setupContracts(signer, networkName)
     setIsReady(success)
   }
@@ -99,14 +99,12 @@ export default function Web3Provider ({ children }) {
   async function getAndSetWeb3ContextWithoutSigner (provider) {
     setIsReady(false)
     const networkName = await getAndSetNetwork(provider)
-    // console.log('here4', networkName)
     const success = await setupContracts(provider, networkName)
     setIsReady(success)
   }
 
   async function getAndSetAccountAndBalance (provider, address) {
     setAccount(address)
-    // console.log('here9', address)
     const signerBalance = await provider.getBalance(address)
     const balanceInEther = ethers.utils.formatEther(signerBalance, 'ether')
     setBalance(balanceInEther)
@@ -114,7 +112,6 @@ export default function Web3Provider ({ children }) {
 
   async function getAndSetNetwork (provider) {
     const { name: network } = await provider.getNetwork()
-    // console.log('here5', network)
     const networkName = networkNames[network]
     setNetwork(networkName)
     return networkName

96de86c6de8ddd0a682abdd869a0a217bb6b28f9
kl3eo <alex@motivation.ru>
2023-06-27 13:11:38 +0300

    add scroll to top button

--- src/components/atoms/ConnectedAccountAddress.js
+++ src/components/atoms/ConnectedAccountAddress.js
@@ -1,9 +1,9 @@
 import { isMobile } from 'react-device-detect'
-import { shortenAddress, shortShortneAddress } from '../../utils/format'
+import { shortenAddress, shortShortenAddress } from '../../utils/format'
 import { chains } from '../../utils/web3'
 import NavItem from './NavItem'
 
 export default function ConnectedAccountAddress ({ account }) {
   const accountUrl = `${chains.expanse.explorers[0].url}/address/${account}`
-  return <NavItem title={isMobile ? shortShortneAddress(account) : shortenAddress(account)} href={accountUrl} openNewTab={true}/>
+  return <NavItem title={isMobile ? shortShortenAddress(account) : shortenAddress(account)} href={accountUrl} openNewTab={true}/>
 }
--- src/components/layout/Base.js
+++ src/components/layout/Base.js
@@ -1,13 +1,33 @@
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import LowOnBalanceTip from '../molecules/LowOnBalanceTip'
 import NavBar from '../molecules/NavBar'
 import NFTModal from '../organisms/NFTModal'
 import NFTModalProvider from '../providers/NFTModalProvider'
 import { Web3Context } from '../providers/Web3Provider'
+import styles from './Button.module.css'
 
 export default function BaseLayout ({ children }) {
   const { network, balance, isReady, hasWeb3 } = useContext(Web3Context)
   const isLowOnEther = balance < 0.1
+  const [showButton, setShowButton] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true)
+      } else {
+        setShowButton(false)
+      }
+    })
+  }, [])
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // for smoothly scrolling
+    })
+  }
+
   return (
     <>
       <NFTModalProvider>
@@ -16,6 +36,11 @@ export default function BaseLayout ({ children }) {
         {children}
         <NFTModal/>
       </NFTModalProvider>
+      {showButton && (
+        <button onClick={scrollToTop} className={styles.back_to_top}>
+          &#8679;
+        </button>
+      )}
     </>
   )
 }
